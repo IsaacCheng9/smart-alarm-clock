581d3261e6dc681c28b111477bd20cbd08562d52
Isaac <isaaccheng9@gmail.com>
2019-10-27 01:04:18 +0000

    Initial commit.

---
+++ smartalarmclock.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def main():
+    show_current_time()
+
+
+def show_current_time():
+    current_datetime = datetime.now()
+    print("Current Date and Time:", current_datetime.strftime(
+          "%Y-%m-%d %H:%M:%S"))
+
+
+# Prevents the code from executing when the script is imported as a module.
+if __name__ == "__main__":
+    main()
