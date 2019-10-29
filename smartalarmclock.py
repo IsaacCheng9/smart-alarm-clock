68eaf2b5cb80533a3aa9235044714547e8cc1c55
Isaac <isaaccheng9@gmail.com>
2019-10-29 02:16:15 +0000

    Added ability to select city, and cleaned up code.

--- smartalarmclock.py
+++ smartalarmclock.py
@@ -10,17 +10,12 @@ import requests
 
 def main():
     """
-    Defines the weather and news APIs to get data from, and then runs the
-    program.
+    Shows the current time, the latest news headlines, and a weather forecast
+    summary.
     """
-    weather_api = ("https://api.openweathermap.org/data/2.5/weather?"
-                   "q=Exeter&units=metric&"
-                   "appid=8bb8c8c3507631f11bb9599e7795a718")
-    news_api = ("https://newsapi.org/v2/top-headlines?"
-                "country=us&apiKey=86a97a39f14a4a1eac894868d7b9726c")
 
     show_time()
-    show_news_weather(weather_api, news_api)
+    show_news_weather()
 
 
 def show_time():
@@ -33,41 +28,45 @@ def show_time():
     print("Current Date and Time:\n    ", current_datetime)
 
 
-def show_news_weather(weather_api: str, news_api: str):
+def show_news_weather():
     """
     Shows the news and weather for the user's chosen city.
-
-    Args:
-        weather_api (str): The weather API URL from OpenWeatherMap.
-        news_api (str): The news API URL from NewsAPI.
     """
+
+    # Gets the API for weather data on user's city.
+    city = input("\nPlease enter your city. ").capitalize()
+    weather_api = ("https://api.openweathermap.org/data/2.5/weather?"
+                   "q={}&appid=8bb8c8c3507631f11bb9599e7795a718"
+                   "&units=metric").format(city)
 
     # Gets weather data using the weather API.
     raw_weather = requests.get(weather_api)
     weather = raw_weather.json()
-    # print(json.dumps(weather, indent=4, sort_keys=True))
     forecast = weather["weather"][0]["main"]
     temp = str(weather["main"]["temp"])
     max_temp = str(weather["main"]["temp_max"])
     min_temp = str(weather["main"]["temp_min"])
     wind = str(weather["wind"]["speed"])
 
+    # Prints a weather forecast summary.
+    print("\nWeather Forecast:\n    " + forecast,
+          "with an average temperature of", temp +
+          "°C.\n    Temperature highs of", max_temp + "°C and lows of",
+          min_temp + "°C.\n    Wind speeds of", wind, "km/h.")
+
+    # Gets latest news using the news API.
+    news_api = ("https://newsapi.org/v2/top-headlines?"
+                "country=us&apiKey=86a97a39f14a4a1eac894868d7b9726c")
+
     # Gets news using the news API.
     raw_news = requests.get(news_api)
     news = raw_news.json()
-    # print(json.dumps(news, indent=4, sort_keys=True))
 
     # Prints the ten news headlines.
     print("\nNews Headlines for Today:")
     for i in range(10):
         articles = str(news["articles"][i]["title"])
         print("    #" + str(i + 1), articles)
-
-    # Prints a weather forecast summary.
-    print("\nWeather Forecast:\n    " + forecast,
-          "with an average temperature of", temp +
-          "°C.\n    Temperature highs of", max_temp + "°C and lows of",
-          min_temp + "°C.\n    Wind speeds of", wind, "km/h.")
 
 
 # Prevents the code from executing when the script is imported as a module.
