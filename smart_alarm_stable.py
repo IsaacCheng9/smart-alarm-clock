ac6492923d1a85ebca671c04eb249667d9af5f69
Isaac <isaaccheng9@gmail.com>
2019-11-18 14:12:39 +0000

    Removed alarms when cancelled, and added HTML loops

--- smart_alarm_stable.py
+++ smart_alarm_stable.py
@@ -15,13 +15,7 @@ import requests
 from flask import Flask, render_template, request
 
 upcoming_alarms = []
-upcoming_alarms_labels = []
 notifications = []
-notification1 = ""
-notification2 = ""
-notification3 = ""
-notification4 = ""
-notification5 = ""
 
 # Initialises Flask for web interface and the scheduler for the alarm.
 app = Flask(__name__)
@@ -42,8 +36,7 @@ def main():
     # Updates weather and news, and shows the last updated time.
     current_datetime = last_updated()
     forecast, temp, max_temp, min_temp, wind = get_weather(api_keys, location)
-    (headline1, headline2, headline3, headline4, headline5, headline6,
-     headline7, headline8, headline9, headline10) = get_news(api_keys, location)
+    headlines = get_news(api_keys, location)
 
     # Enables alarm functionality.
     upcoming_alarms, displayed_alarms = set_alarm()
@@ -51,18 +44,11 @@ def main():
 
     # Returns the variables to the HTML file to render webpage.
     return render_template("home.html", current_datetime=current_datetime,
-                           notification1=notification1,
-                           notification2=notification2,
-                           notification3=notification3,
-                           notification4=notification4,
-                           notification5=notification5,
+                           notifications=notifications,
                            forecast=forecast, temp=temp,
                            max_temp=max_temp, min_temp=min_temp, wind=wind,
-                           headline1=headline1, headline2=headline2,
-                           headline3=headline3, headline4=headline4,
-                           headline5=headline5, headline6=headline6,
-                           headline7=headline7, headline8=headline8,
-                           headline9=headline9, headline10=headline10,
+                           headlines=headlines,
+                           upcoming_alarms=upcoming_alarms,
                            displayed_alarms=displayed_alarms)
 
 
@@ -128,30 +114,15 @@ def get_notifications(new_notification: str):
         new_notification (str): Stores the new notification to be added.
 
     Returns:
-        notifications# (str): Stores the numbered notification (depending on
-                              #).
+        notifications (list): Stores a list of notifications to be displayed.
     """
 
-    global notification1, notification2, notification3, notification4
-    global notification5
+    # Adds a new notification to the notification list with a timestamp.
+    notification_input = (datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ": "
+                          + new_notification)
+    notifications.insert(0, notification_input)
 
-    # Adds new notification to the list.
-    notifications.append(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ": "
-                         + new_notification)
-
-    # Gets the latest five notifications.
-    try:
-        notification1 = notifications[-1]
-        notification2 = notifications[-2]
-        notification3 = notifications[-3]
-        notification4 = notifications[-4]
-        notification5 = notifications[-5]
-    # Prevents crashing when there are no notifications on program startup.
-    except IndexError:
-        pass
-
-    return (notification1, notification2, notification3, notification4,
-            notification5)
+    return notifications
 
 
 def get_weather(api_keys: dict, location: dict) -> str:
@@ -200,8 +171,11 @@ def get_news(api_keys: dict, location: dict) -> str:
         location (dict): Stores the location to enable local news and weather.
 
     Returns:
-        headline# (str): Stores the numbered headline (depending on #).
+        headlines (list): Stores the list of headlines to be displayed.
     """
+
+    # Declares list of headlines to be displayed.
+    headlines = []
 
     # Gets latest news using the news API.
     news_key = api_keys["news"]
@@ -213,23 +187,15 @@ def get_news(api_keys: dict, location: dict) -> str:
     raw_news = requests.get(news_api)
     news = raw_news.json()
 
-    # Gets and assigns top ten headlines.
-    headline1 = str(news["articles"][0]["title"])
-    headline2 = str(news["articles"][1]["title"])
-    headline3 = str(news["articles"][2]["title"])
-    headline4 = str(news["articles"][3]["title"])
-    headline5 = str(news["articles"][4]["title"])
-    headline6 = str(news["articles"][5]["title"])
-    headline7 = str(news["articles"][6]["title"])
-    headline8 = str(news["articles"][7]["title"])
-    headline9 = str(news["articles"][8]["title"])
-    headline10 = str(news["articles"][9]["title"])
+    # Gets and appends top ten headlines to the list of headlines.
+    for i in range(10):
+        headline = "#" + str(i + 1) + ": " + str(news["articles"][i]["title"])
+        headlines.append(headline)
 
     # Adds notification that news was updated.
     get_notifications("News has been updated.")
 
-    return (headline1, headline2, headline3, headline4, headline5, headline6,
-            headline7, headline8, headline9, headline10)
+    return headlines
 
 
 def alert_alarm(alarm_time: str, alarm_label: str, alarm_repeat: str):
@@ -242,6 +208,8 @@ def alert_alarm(alarm_time: str, alarm_label: str, alarm_repeat: str):
         alarm_repeat (str): Whether the alarm repeats or not.
     """
 
+    global upcoming_alarms
+
     # Alerts user about their alarm via voiceover.
     text_to_speech = pyttsx3.init()
     text_to_speech.say(("Your alarm with label", alarm_label, "is going off."))
@@ -249,6 +217,8 @@ def alert_alarm(alarm_time: str, alarm_label: str, alarm_repeat: str):
 
     # Prints when alarm is going off in console for debugging purposes.
     print("\nYour alarm with label", alarm_label, "is going off!")
+
+    upcoming_alarms.pop(0)
 
     if alarm_repeat:
         new_date = int(alarm_time[8:10]) + 1
@@ -272,7 +242,7 @@ def set_alarm() -> list:
         displayed_alarms (str): A string list of upcoming alarms to display.
     """
 
-    global upcoming_alarms, upcoming_alarms_labels
+    global upcoming_alarms
     displayed_alarms = ""
 
     # Gets the alarm time from the new alarm input box and calculates delay.
@@ -302,9 +272,9 @@ def set_alarm() -> list:
         upcoming_alarms.append(alarm_input)
         upcoming_alarms = sorted(upcoming_alarms)
 
-    # Creates the displayed list of alarms.
+    """# Creates the displayed list of alarms.
     for alarm_input in upcoming_alarms:
-        displayed_alarms += "\n" + alarm_input
+        displayed_alarms += "\n" + alarm_input"""
 
     return upcoming_alarms, displayed_alarms
 
@@ -314,7 +284,7 @@ def cancel_alarm():
     Allows the user to cancel an alarm.
 
     Args:
-        upcoming_alarms (str): A list of the upcoming alarms.
+        upcoming_alarms (list): A list of the upcoming alarms.
     """
 
     # Gets the time for the alarm to cancel.
@@ -327,6 +297,8 @@ def cancel_alarm():
         for event in alarm.queue:
             epoch = event[0]
             if epoch == alarm_cancel_epoch:
+                index = alarm.queue.index(event)
+                del upcoming_alarms[index]
                 alarm.cancel(event)
 
 
