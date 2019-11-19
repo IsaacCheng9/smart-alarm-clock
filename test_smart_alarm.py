459dde829ccaa988d4a917748aeb6a2341160ed5
Isaac <isaaccheng9@gmail.com>
2019-11-19 12:01:13 +0000

    Updated unit testing

--- test_smart_alarm.py
+++ test_smart_alarm.py
@@ -1,42 +1,52 @@
 import unittest
-import smart_alarm
+
+from smart_alarm import (cancel_alarm, get_news, last_updated, parse_configs,
+                         set_alarm)
 
 
 class TestParseConfigs(unittest.TestCase):
-    # Returns True if file is lowercase, else returns False.
-    def test_file_capitalisation(self):
-        self.assertTrue(smart_alarm.file.islower())
-        self.assertFalse(smart_alarm.file.islower())
+    # Checks if the config file output is a tuple of dictionaries.
+    def test_config_file(self):
+        self.assertIsInstance(parse_configs(), tuple)
 
 
 class TestLastUpdated(unittest.TestCase):
-    # Checks if current_datetime is a datetime variable.
-    def test_datetime_type(self):
-        self.assertIsInstance(smart_alarm.current_datetime, datetime)
+    # Checks if current_datetime is a string variable.
+    def test_current_datetime_type(self):
+        self.assertIsInstance(last_updated(), str)
 
 
-class TestGetNotifications(unittest.TestCase):
+"""class TestGetNotifications(unittest.TestCase):
     # Checks if news_notifications is a list variable.
     def test_datetime_type(self):
-        self.assertIsInstance(smart_alarm.news_notifications, list)
+        self.assertIsInstance(get_news(api_keys, location), list)
 
 
 class TestGetNews(unittest.TestCase):
     # Raises error if two lists aren't equal.
     def test_news_headlines(self):
-        self.assertListEqual(smart_alarm.old_headlines, smart_alarm.headlines)
+        self.assertListNotEqual(get_news(api_keys, location).old_headlines, get_news(
+            api_keys, location).headlines)
 
 
 class TestGetAlarmInputs(unittest.TestCase):
     # Passes if a ValueError is raised with input of 'x'.
-    def test_values(self):
-        self.assertRaises(ValueError, smart_alarm.alarm_time, "x")
+
+    def test_alarm_values(self):
+        self.assertRaises(ValueError, get_alarms().alarm_time, "x")
 
 
 class TestSetAlarm(unittest.TestCase):
     # Checks if upcoming_alarms is a list variable.
     def test_datetime_type(self):
-        self.assertIsInstance(smart_alarm.upcoming_alarms, list)
+        self.assertIsInstance(set_alarm(
+            alarm_time, alarm_label, alarm_repeat, format_time).upcoming_alarms, list)
+
+
+class TestCancelAlarm(unittest.TestCase):
+    # Passes if a ValueError is raised with input of 'x'.
+    def test_cancel_alarm_values(self):
+        self.assertRaises(ValueError, cancel_alarm("x").alarm_time)"""
 
 
 if __name__ == "__main__":
